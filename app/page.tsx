efcce5867b4f199fdadbd3939f56d55b55e4b6b9
kelmyrl <betykelmi@gmail.com>
2026-09-10 14:28:00 -0500

    added the page for the frontend

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,16 @@ export default async function Page() {
   const cookieStore = await cookies()
   const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
+  const { data: sales } = await supabase.from('sales').select()
 
   return (
-    <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
-      ))}
-    </ul>
+    <div>
+      <h1>Sales</h1>
+      <ul>
+        {sales?.map((sale) => (
+          <li key={sale.id}>{sale.sales_date}, {sale.degC}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
